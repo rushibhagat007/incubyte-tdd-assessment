84e59f9237b8c9f7b1c804a793d0096d1ed05908
rushibhagat007 <rushibhagat007@gmail.com>
2024-02-25 19:42:29 +0530

    11 - Refactored StringCalculator.add method for add custom delimiter

--- src/main/java/com/incubyte/tdd/assessment/StringCalculator.java
+++ src/main/java/com/incubyte/tdd/assessment/StringCalculator.java
@@ -7,7 +7,16 @@ public class StringCalculator {
 			return 0;
 		}
 
-		String[] nums = numbers.split("[,\n]");
+		String delimiter = ",";
+	    String input = numbers;
+
+	    if (numbers.startsWith("//")) {
+	        int index = numbers.indexOf("\n");
+	        delimiter = numbers.substring(2, index);
+	        input = numbers.substring(index + 1);
+	    }
+
+	    String[] nums = input.split("[,\n" + delimiter + "]");
 	    int sum = 0;
 	    for (String num : nums) {
 	        sum += Integer.parseInt(num);
