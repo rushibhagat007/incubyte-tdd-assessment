c58f057bdc572919358181e466187b4247d8557b
rushibhagat007 <rushibhagat007@gmail.com>
2024-02-25 18:51:10 +0530

    04 - Added test cases testOneNumber and testTwoNumbers in StringCalculatorTest

--- src/test/java/com/incubyte/tdd/assessment/StringCalculatorTest.java
+++ src/test/java/com/incubyte/tdd/assessment/StringCalculatorTest.java
@@ -10,4 +10,16 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator();
         assertEquals(0, calculator.add(""));
     }
+	
+	@Test
+	public void testOneNumber() {
+	    StringCalculator calculator = new StringCalculator();
+	    assertEquals(1, calculator.add("1"));
+	}
+	
+	@Test
+    public void testAddTwoNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("1,2"));
+    }
 }
