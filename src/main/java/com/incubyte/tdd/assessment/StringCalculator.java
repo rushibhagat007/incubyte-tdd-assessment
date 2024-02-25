c82ec49f3cefa6340579017416ef225b2a1f7b9e
rushibhagat007 <rushibhagat007@gmail.com>
2024-02-25 19:01:16 +0530

    05 - Implemented logic in StringCalculator.add method to handle one and two numbers

--- src/main/java/com/incubyte/tdd/assessment/StringCalculator.java
+++ src/main/java/com/incubyte/tdd/assessment/StringCalculator.java
@@ -3,9 +3,15 @@ package com.incubyte.tdd.assessment;
 public class StringCalculator {
 
 	public int add(String numbers) {
-    	 if (numbers.isEmpty()) {
-             return 0;
-         }
-         return -1;
+		if (numbers.isEmpty()) {
+			return 0;
+		}
+		
+		String[] nums = numbers.split(",");
+		if (nums.length == 1) {
+			return Integer.parseInt(nums[0]);
+		}
+		return Integer.parseInt(nums[0]) + Integer.parseInt(nums[1]);
 	}
+
 }
