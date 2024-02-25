aaee4f0acbaf8b99982095ff91b7dc166df370f7
rushibhagat007 <rushibhagat007@gmail.com>
2024-02-25 20:14:56 +0530

    17 - Refactored StringCalculator.add method for add custom delimiter of any length

--- src/main/java/com/incubyte/tdd/assessment/StringCalculator.java
+++ src/main/java/com/incubyte/tdd/assessment/StringCalculator.java
@@ -11,27 +11,34 @@ public class StringCalculator {
 		}
 
 		String delimiter = ",";
-	    String input = numbers;
+		String input = numbers;
 
-	    if (numbers.startsWith("//")) {
-	        int index = numbers.indexOf("\n");
-	        delimiter = numbers.substring(2, index);
-	        input = numbers.substring(index + 1);
-	    }
+		if (numbers.startsWith("//")) {
+			int start = numbers.indexOf("[");
+			int end = numbers.indexOf("]");
+			if (start != -1 && end != -1) {
+				delimiter = numbers.substring(start + 1, end);
+			} else {
+				delimiter = numbers.substring(2, 3);
+			}
+			input = numbers.substring(numbers.indexOf("\n") + 1);
+		}
 
-	    String[] nums = input.split("[,\n" + delimiter + "]");
-	    
-	    int sum = 0;
-	    List<Integer> negativeNumbers = new ArrayList<>();
+		String[] nums = input.split("[,\n" + delimiter + "]");
+		int sum = 0;
+		List<Integer> negativeNumbers = new ArrayList<>();
 
-        for (String num : nums) {
-            int n = Integer.parseInt(num);
-            if (n < 0) {
-            	negativeNumbers.add(n);
-			} else if (n <= 1000) {
-				sum += n;
+		for (String num : nums) {
+			if (!num.isEmpty()) {
+				int n = Integer.parseInt(num);
+				if (n < 0) {
+					negativeNumbers.add(n);
+				} else if (n <= 1000) {
+					sum += n;
+				}
 			}
 		}
+
 		if (!negativeNumbers.isEmpty()) {
 			throw new IllegalArgumentException("Negative numbers not allowed: " + negativeNumbers);
 		}
