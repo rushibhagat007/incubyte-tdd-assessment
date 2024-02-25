ab3f7822bf2e0ab03432eef47ac1aded81aa6649
rushibhagat007 <rushibhagat007@gmail.com>
2024-02-25 19:51:55 +0530

    13 - Refactored StringCalculator.add method for add negative numbers

--- src/main/java/com/incubyte/tdd/assessment/StringCalculator.java
+++ src/main/java/com/incubyte/tdd/assessment/StringCalculator.java
@@ -1,5 +1,8 @@
 package com.incubyte.tdd.assessment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
 	public int add(String numbers) {
@@ -17,10 +20,22 @@ public class StringCalculator {
 	    }
 
 	    String[] nums = input.split("[,\n" + delimiter + "]");
+	    
 	    int sum = 0;
-	    for (String num : nums) {
-	        sum += Integer.parseInt(num);
-	    }
-	    return sum;
-	}
+	    List<Integer> negativeNumbers = new ArrayList<>();
+
+        for (String num : nums) {
+            int n = Integer.parseInt(num);
+            if (n < 0) {
+            	negativeNumbers.add(n);
+            }
+            sum += n;
+        }
+
+        if (!negativeNumbers.isEmpty()) {
+            throw new IllegalArgumentException("Negative numbers not allowed: " + negativeNumbers);
+        }
+
+        return sum;
+    }
 }
